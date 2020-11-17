9e5b9b19eef73e39f3eb16672b2bf3c2f27e7d4c
llukkah <llukkah.rey@gmail.com>
2020-11-17 15:03:22 -0500

    cleaned up code

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -9,25 +9,19 @@ export default function News() {
         const newsURL = "https://newsapi.org/v2/top-headlines";
         const fetchData = async() => {
         const result = await axios(`${newsURL}?country=us&apiKey=${apiKey}`);
-
+        //as discussed when time ran out next steps:
+        //-populate the setallNews variable with the results(result.data.articles) from the axios call.
         }
     }))
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//afterwards, in the return  section to display our results, 
+// we will map through the allNews array
+// then set the articles in each article card with the info from the API
+// {allNews.map((news) => (
+//     <div>
+//         <a href={news.url}><{news.url}</a>
+//         <p>{news.description}</p>
+//         <p>{news.source.name}</p>
+//     </div>
+// ))}
 }
